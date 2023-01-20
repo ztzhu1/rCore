d7cbd36c79bc7c6bf77baaf9185bb5d299c9b5c7
ztzhu <zhuzitian@zju.edu.cn>
2023-01-20 20:36:18 +0800

    sbi runtime

---
+++ os/src/sbi.rs
@@ -0,0 +1,53 @@
+#![allow(unused)]
+
+const SBI_SET_TIMER: usize = 0;
+const SBI_CONSOLE_PUTCHAR: usize = 1;
+const SBI_CONSOLE_GETCHAR: usize = 2;
+const SBI_CLEAR_IPI: usize = 3;
+const SBI_SEND_IPI: usize = 4;
+const SBI_REMOTE_FENCE_I: usize = 5;
+const SBI_REMOTE_SFENCE_VMA: usize = 6;
+const SBI_REMOTE_SFENCE_VMA_ASID: usize = 7;
+const SBI_SHUTDOWN: usize = 8;
+
+const EXIT_SUCCESS: usize = 0;
+const EXIT_FAILURE: usize = 1;
+
+use core::arch::asm;
+
+#[inline(always)]
+fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+    let mut ret;
+    unsafe {
+        asm!(
+            "ecall",
+            inlateout("x10") arg0 => ret,
+            in("x11") arg1,
+            in("x12") arg2,
+            in("x17") which,
+        )
+    }
+    ret
+}
+
+pub fn console_putchar(ch: usize) {
+    sbi_call(SBI_CONSOLE_PUTCHAR, ch as usize, 0, 0);
+}
+
+pub fn console_getchar() -> usize {
+    sbi_call(SBI_CONSOLE_GETCHAR, 0, 0, 0)
+}
+
+fn shutdown(ecode: usize) -> ! {
+    println!("RustSBI-QEMU shutdown!");
+    sbi_call(SBI_SHUTDOWN, ecode, 0, 0);
+    panic!("It should shutdown!");
+}
+
+pub fn exit_success() -> ! {
+    shutdown(EXIT_SUCCESS);
+}
+
+pub fn exit_failure() -> ! {
+    shutdown(EXIT_FAILURE);
+}
