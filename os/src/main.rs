9f017523aa2375c0c9d39a1af4219324c3c7a952
ztzhu <zhuzitian@zju.edu.cn>
2023-01-20 17:09:44 +0800

    Jump to entry and clear bss

--- os/src/main.rs
+++ os/src/main.rs
@@ -8,6 +8,18 @@ use core::arch::global_asm;
 
 global_asm!(include_str!("entry.s"));
 
+#[no_mangle]
 fn rust_main() {
+    clear_bss();
+    loop {}
+}
 
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|x| unsafe {
+        (x as *mut u8).write_volatile(0);
+    });
 }
