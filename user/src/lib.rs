74aa2b02f5e9fe24ec6f0457f60a9515afde9d2e
ztzhu <zhuzitian@zju.edu.cn>
2023-01-21 11:19:55 +0800

    lib functions

---
+++ user/src/lib.rs
@@ -0,0 +1,43 @@
+#![no_std]
+#![allow(unreachable_code)]
+#![feature(linkage)]
+#![feature(panic_info_message)]
+
+#[macro_use]
+pub mod console;
+mod lang_items;
+mod syscall;
+
+#[no_mangle]
+#[link_section = ".text.entry"]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    exit(main());
+    panic!("unreachable after sys_exit!");
+}
+
+#[linkage = "weak"]
+#[no_mangle]
+fn main() -> i32 {
+    panic!("Cannot find main!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|addr| unsafe {
+        (addr as *mut u8).write_volatile(0);
+    });
+}
+
+use syscall::*;
+
+pub fn write(fd: usize, buf: &[u8]) -> isize {
+    sys_write(fd, buf)
+}
+
+pub fn exit(exit_code: i32) -> ! {
+    sys_exit(exit_code);
+}
