d5f4f532383e091144283c2bf889404015bc14f3
ztzhu1 <zhuzitian@zju.edu.cn>
2023-02-09 23:48:56 +0800

    Handle app name

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -5,6 +5,8 @@ use crate::mm::memory_set::{MapPermission, MemorySet, KERNEL_SPACE};
 use crate::safe_refcell::SafeRefCell;
 use crate::task::{TaskContext, TaskControlBlock, TaskStatus};
 use crate::trap::TrapContext;
+use alloc::vec::Vec;
+use lazy_static::lazy_static;
 
 pub fn examine_app_id_valid(app_id: usize) {
     let n = app_num();
@@ -55,4 +57,39 @@ pub fn app_data(app_id: usize) -> &'static [u8] {
         let end = (_num_app as *const usize).add(app_id + 2).read_volatile();
         core::slice::from_raw_parts(start as *const u8, end - start)
     }
+}
+
+pub fn app_data_by_name(app_name: &str)-> Option<&'static [u8]> {
+    let n = app_num();
+    let app_id = APP_NAMES.binary_search(&app_name).ok()?;
+    Some(app_data(app_id))
+}
+
+pub fn app_name(app_id: usize) -> &'static str {
+    examine_app_id_valid(app_id);
+    APP_NAMES[app_id]
+}
+
+lazy_static! {
+    static ref APP_NAMES: Vec<&'static str> = {
+        let mut names: Vec<&str> = Vec::new();
+        extern "C" {
+            fn _app_names();
+        }
+        let n = app_num();
+        let mut start = _app_names as *const u8;
+        let mut end = start;
+        unsafe {
+            for i in 0..n {
+                while end.read_volatile() != 0 {
+                    end = end.add(1);
+                }
+                let name_slice = core::slice::from_raw_parts(start, end as usize - start as usize);
+                let name = core::str::from_utf8(name_slice).unwrap();
+                names.push(name);
+                start = end.add(1);
+            }
+        }
+        names
+    };
 }
